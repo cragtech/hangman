44fc43c73e130ac42a2c82104b22a0d19fcc2ab8
cragtech <cragtechnology@gmail.com>
2026-01-11 21:02:33 -0700

    removed all files

--- .next/dev/static/chunks/hangman_app_favicon_ico_mjs_151a0465._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/4b3af_next_dist_c6b19601._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/hangman_app_layout_tsx_74b5f016._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__7c4999ea._.css"
-],
-    source: "dynamic"
-});
